7d9265953178d3b7dd23dd2e807ae704646a7f7e
IjodbekMehmonaliyev03 <IjodbekMehmonaliyev@gmail.com>
2022-05-30 17:03:14 +0500

    contact is finished main page3/4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Services from "./Components/Services/Services";
 import Experience from "./Components/Experience/Experience";
 import Works from "./Components/Works/Works";
 import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
 function App() {
   return (
     <div className="App">
@@ -14,6 +16,8 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Testimonials />
+      <Contact />
     </div>
   );
 }
